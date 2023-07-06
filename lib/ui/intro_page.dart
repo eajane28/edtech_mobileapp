e051c7a29ed290fa791112852a6862ca95ad2470
eajane28 <[200~rheajeaj@gmail.com>
2023-07-06 13:42:39 +0800

    Intro page done

--- lib/ui/intro_page.dart
+++ lib/ui/intro_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-//import 'package:onboarding/onboarding.dart';
+import 'package:onboarding/onboarding.dart';
 
 class IntroPage extends StatefulWidget {
   const IntroPage({Key? key}) : super(key: key);
@@ -13,135 +13,208 @@ class _IntroPageState extends State<IntroPage> {
   late int index;
   final onboardingPagesList = [
     //page1
-    Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Align(
-            alignment: Alignment.center,
-            child: Image.asset(
-              'assets/Cool Kids Long Distance Relationship.png',
-              width: 375,
-              height: 264,
-            )),
-        const Text(
-          'Learn anytime and anywhere',
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            color: Color(0xFF3B3936),
-            fontSize: 24,
-            fontWeight: FontWeight.w500,
+    PageModel(
+      widget: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Align(
+              alignment: Alignment.center,
+              child: Image.asset(
+                'assets/Cool Kids Long Distance Relationship.png',
+                width: 375,
+                height: 264,
+              )),
+          const Text(
+            'Learn anytime and anywhere',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Color(0xFF3B3936),
+              fontSize: 24,
+              fontWeight: FontWeight.w500,
+            ),
           ),
-        ),
-        const Text(
-          'Quarantine is the perfect time to spend your day learning something new, from anywhere!',
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            color: Color(0xFF3B3936),
-            fontSize: 14,
+          const Text(
+            'Quarantine is the perfect time to spend your day learning something new, from anywhere!',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Color(0xFF3B3936),
+              fontSize: 14,
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     ),
     //page2
-    Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Align(
-            alignment: Alignment.center,
-            child: Image.asset(
-              'assets/Cool Kids Staying Home.png',
-              width: 375,
-              height: 264,
-            )),
-        const Text(
-          'Find a course for you',
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            color: Color(0xFF3B3936),
-            fontSize: 24,
-            fontWeight: FontWeight.w500,
+    PageModel(
+      widget: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Align(
+              alignment: Alignment.center,
+              child: Image.asset(
+                'assets/Cool Kids Staying Home.png',
+                width: 375,
+                height: 264,
+              )),
+          const Text(
+            'Find a course for you',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Color(0xFF3B3936),
+              fontSize: 24,
+              fontWeight: FontWeight.w500,
+            ),
           ),
-        ),
-        const Text(
-          'Quarantine is the perfect time to spend your day learning something new, from anywhere!',
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            color: Color(0xFF3B3936),
-            fontSize: 14,
+          const Text(
+            'Quarantine is the perfect time to spend your day learning something new, from anywhere!',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Color(0xFF3B3936),
+              fontSize: 14,
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     ),
     //page3
-    Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Align(
-            alignment: Alignment.center,
-            child: Image.asset(
-              'assets/Cool Kids High Tech.png',
-              width: 375,
-              height: 264,
-            )),
-        const Text(
-          'Improve your skills',
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            color: Color(0xFF3B3936),
-            fontSize: 24,
-            fontWeight: FontWeight.w500,
+    PageModel(
+      widget: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Align(
+              alignment: Alignment.center,
+              child: Image.asset(
+                'assets/Cool Kids High Tech.png',
+                width: 375,
+                height: 264,
+              )),
+          const Text(
+            'Improve your skills',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Color(0xFF3B3936),
+              fontSize: 24,
+              fontWeight: FontWeight.w500,
+            ),
           ),
-        ),
-        const Text(
-          'Quarantine is the perfect time to spend your day learning something new, from anywhere!',
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            color: Color(0xFF3B3936),
-            fontSize: 14,
+          const Text(
+            'Quarantine is the perfect time to spend your day learning something new, from anywhere!',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Color(0xFF3B3936),
+              fontSize: 14,
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     ),
   ];
+
+  @override
+  void initState() {
+    super.initState();
+    index = 0;
+  }
+
+  // PageModel? skipPage() {
+  //   int nextIndex = index + 1;
+  //   if (nextIndex < onboardingPagesList.length) {
+  //     return onboardingPagesList[nextIndex];
+  //   } else {
+  //     return null; // No next page, return null
+  //   }
+  // }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-            height: MediaQuery.sizeOf(context).height,
-            width: MediaQuery.sizeOf(context).width,
-            alignment: Alignment.center,
-            child: PageView(
-              children: onboardingPagesList,
-            )));
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        actions: const [
+          Padding(padding: EdgeInsets.only(right: 16),
+          child: Text('Skip'),)
+        ],
+      ),
+      body: Onboarding(
+        pages: onboardingPagesList,
+        onPageChange: (int pageIndex) {
+          index = pageIndex;
+        },
+        startPageIndex: 0,
+        footerBuilder: (context, dragDistance, pagesLength, setIndex) {
+          return Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(bottom: 50.0, right: 80.0),
+                child: Align(
+                  alignment: Alignment.center,
+                  child: CustomIndicator(
+                    netDragPercent: dragDistance,
+                    pagesLength: pagesLength,
+                    indicator: Indicator(
+                      indicatorDesign: IndicatorDesign.line(
+                        lineDesign: LineDesign(
+                          lineType: DesignType.line_nonuniform,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              index == pagesLength - 1
+                  ? _goButton()
+                  : _skipButton(setIndex: setIndex)
+            ],
+          );
+        },
+      ),
+    );
   }
 }
 
-// @override
-// void initState() {
-//   super.initState();
-//   materialButton = _skipButton();
-//   index = 0;
-// }
+_skipButton({required void Function(int index) setIndex}) {
+  GestureDetector(
+    onTap: () {
+      const Text('Sign in');
+      setIndex(2);
+    },
+  );
+  return Padding(
+    padding: const EdgeInsets.all(45.0),
+    child: Container(
+      width: 311,
+      height: 56,
+      decoration: BoxDecoration(
+        color: Colors.orange,
+        borderRadius: BorderRadius.circular(16),
+      ),
+      alignment: Alignment.center,
+      child: const Text(
+        style: TextStyle(color: Colors.white),
+        'Next',
+      ),
+    ),
+  );
+}
 
-// Material _skipButton({void Function(int)? setIndex}) {
-//     return Material(
-//       borderRadius: defaultSkipButtonBorderRadius,
-//       color: defaultSkipButtonColor,
-//       child: InkWell(
-//         borderRadius: defaultSkipButtonBorderRadius,
-//         onTap: () {
-//           if (setIndex != null) {
-//             index = 2;
-//             setIndex(2);
-//           }
-//         },
-//         child: const Padding(
-//           padding: defaultSkipButtonPadding,
-//           child: Text(
-//             'Skip',
-//             style: defaultSkipButtonTextStyle,
-//           ),
-//         ),
-//       ),
-//     );
-//   }
+_goButton() {
+ 
+  return Padding(
+    padding: const EdgeInsets.all(45.0),
+    child: Container(
+      width: 311,
+      height: 56,
+      decoration: BoxDecoration(
+        color: Colors.orange,
+        borderRadius: BorderRadius.circular(16),
+      ),
+      alignment: Alignment.center,
+      child: const Text(
+        style: TextStyle(color: Colors.white),
+        'Let\'s Start',
+      ),
+    ),
+  );
+}
